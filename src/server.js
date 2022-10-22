531eaf8e31d93b6e0e3f2ba1fedafb5ae76190d9
JongminChoi <joeychoi7624@gmail.com>
2022-10-23 03:04:29 +0900

    [ADD] mongodb Video Schema

--- src/server.js
+++ src/server.js
@@ -1,24 +1,9 @@
 import "dotenv/config";
 import "./db";
-import express from "express";
-import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
-import videoRouter from "./routers/videoRouter";
-import userRouter from "./routers/userRouter";
+import "./models/Video";
+import app from "./app.js";
 
 const PORT = process.env.GLOBAL_PORT;
-
-const app = express();
-const logger = morgan("dev");
-
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
-app.use(logger);
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/", globalRouter);
-app.use("/videos", videoRouter);
-app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);

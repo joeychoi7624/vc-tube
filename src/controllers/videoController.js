3e378f77366ac407170fbda6d6afd4faa4d0acfe
JongminChoi <joeychoi7624@gmail.com>
2022-10-25 21:35:51 +0900

    [ADD] Search Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video, { formatHashtags } from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -58,4 +58,23 @@ export const postUpload = async (req, res) => {
   }
 
   return res.redirect("/");
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };

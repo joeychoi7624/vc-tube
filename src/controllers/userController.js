fac77d644754cec15a03c7ab39014c06aae2698d
JongminChoi <joeychoi7624@gmail.com>
2022-11-01 15:25:19 +0900

    [ADD] User join validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,21 @@ export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
 };
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/emailrs is already taken.",
+    });
+  }
   await User.create({
     name,
     username,

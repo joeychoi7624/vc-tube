eaff4ebe114b30ce94733526bd2c3579b165863a
JongminChoi <joeychoi7624@gmail.com>
2022-11-05 02:37:33 +0900

    [ADD] user edit template & protector and public only middlewares for wrong url attempt

--- src/middlewares/localsMiddleware.js
+++ src/middlewares/localsMiddleware.js
@@ -1,8 +1,24 @@
 const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "JoeyTube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
 };
 
 export default localsMiddleware;
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};

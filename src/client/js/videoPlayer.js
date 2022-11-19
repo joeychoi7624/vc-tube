2c65645d1ba5ba2d74d3a6dec9a8fb3b81a5362b
JongminChoi <joeychoi7624@gmail.com>
2022-11-20 02:47:20 +0900

    [FIXED] video time duration Error

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -105,7 +105,8 @@ const handleEnded = () => {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("canplay", handleLoadedMetadata);
+handleLoadedMetadata();
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);

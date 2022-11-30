69ad72fba27140a34bfe3e5481fe53bc4f311d9f
JongminChoi <joeychoi7624@gmail.com>
2022-12-01 00:30:48 +0900

    fixed cors problem

--- src/app.js
+++ src/app.js
@@ -20,8 +20,11 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
